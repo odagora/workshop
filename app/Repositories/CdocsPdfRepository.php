26501f924383db057f2efc3027d9b105c17f4c22
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-01-16 17:04:41 -0500

    fixed cdocs bug in CdocsPdfRepository

--- app/Repositories/CdocsPdfRepository.php
+++ app/Repositories/CdocsPdfRepository.php
@@ -15,7 +15,8 @@ trait CdocsPdfRepository {
                 $make = Make::find($cdoc->make);
                 $type = Type::find($cdoc->type);
                 $license = Cdocs::find($cdoc->license);
-                $pdf = \PDF::loadView('cdocs.pdf', compact('cdoc', 'make', 'type'))->setPaper('Letter')->setOption('margin-top', 1)->setOption('margin-left', 6)->setOption('margin-right', 6)->setOption('footer-spacing', 2);
+                $doc = $cdoc->doc_number + 762;
+                $pdf = \PDF::loadView('cdocs.pdf', compact('cdoc', 'make', 'type','doc'))->setPaper('Letter')->setOption('margin-top', 1)->setOption('margin-left', 6)->setOption('margin-right', 6)->setOption('footer-spacing', 2);
 
                 return $pdf;
         }
