91aac057f8c7564c206b27974ce10fb7c551f0c7
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-01-16 17:01:54 -0500

    fixed cdocs pdf generation bug

--- app/Http/Controllers/CdocsPdfController.php
+++ app/Http/Controllers/CdocsPdfController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Repositories\cdocsPdfRepository;
+use App\Repositories\CdocsPdfRepository;
 use App\Cdocs;
 
 class CdocsPdfController extends Controller
