4f459191bb41ba3c2f3239480fe56f15fca4dca7
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-03-16 14:14:39 -0500

    changed database and cloudder settings for local dev

--- config/cloudder.php
+++ config/cloudder.php
@@ -12,12 +12,12 @@ return [
     |
     */
 
-    'cloudName'  => 'hg9i4mfep',
-    'baseUrl'    => 'http://res.cloudinary.com/hg9i4mfep',
-    'secureUrl'  => 'https://res.cloudinary.com/hg9i4mfep',
-    'apiBaseUrl' => 'https://api.cloudinary.com/v1_1/hg9i4mfep',
-    'apiKey'     => '163776537282497',
-    'apiSecret'  => 'sHMQyf6jx7FjnXxI-IU34SKVd5w',
+    'cloudName'  => env('CLOUDINARY_CLOUD_NAME'),
+    'baseUrl'    =>  env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
+    'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
+    'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
+    'apiKey'     =>  env('CLOUDINARY_API_KEY'),
+    'apiSecret'  => env('CLOUDINARY_API_SECRET'),
 
     'scaling'    => [
         'format' => 'png',
