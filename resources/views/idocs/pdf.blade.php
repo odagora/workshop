aef3d0e06b4b4439846c49f8f1341fcd84b31f9a
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-04-24 16:49:03 -0500

    changed qdocs and idocs pdf display layout

--- resources/views/idocs/pdf.blade.php
+++ resources/views/idocs/pdf.blade.php
@@ -6,55 +6,103 @@
 		<img src="{{asset('img/logo.png')}}" class="img-responsive" alt="logo servitalleres">
 	</div>
 	<div class="col-xs-6 col-sm-6 col-md-6 text-right text-danger doc-number">
-		<p><strong>No. {{ $doc }}</strong></p>
+		<div class="col-xs-5 col-sm-5 col-md-5 col-xs-offset-7 text-center panel panel-default">
+			<p><strong>No. {{ $doc }}</strong></p>
+		</div>
 	</div>
 </div>
 <div class="row">
-	<h3 class="text-center text-uppercase"><strong>Inspección Visual de Mecánica y Colisión @if($idoc->status == 'cancelled')<span class="text-danger">- ANULADO -</span>@endif </strong></h3>
-	<hr>
+	<h3 class="text-center text-uppercase" style="margin-bottom: 5px;"><strong>Inspección Visual de Mecánica y Colisión @if($idoc->status == 'cancelled')<span class="text-danger">- ANULADO -</span>@endif </strong></h3>
 </div>
-<div class="row ">
-	<div class="col-xs-4 col-sm-4 col-md-4">
-		<div class="col-xs-4 col-sm-4 col-md-4">
-			<p><strong>Fecha:</strong></p>
-			<p><strong>Asesor:</strong></p>
+<div class="row well quote">
+	<div class="col-xs-4 col-sm-4 col-md-4" style="padding: 0">
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="col-xs-6 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Fecha:</strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-8 col-md-8 text-center panel panel-default">
+				<h6>{{ date('d/m/Y', strtotime($idoc->created_at)) }}</h6>
+			</div>
 		</div>
-		<div class="col-xs-8 col-sm-8 col-md-8">
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ date('d/m/Y', strtotime($idoc->created_at)) }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $idoc->e_firstname }} {{ $idoc->e_lastname }}</p>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="col-xs-3 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Asesor:</strong></h6>
+			</div>
+			<div class="col-xs-9 col-sm-8 col-md-8 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $idoc->e_firstname }} {{ $idoc->e_lastname }}</h6>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-4 col-sm-4 col-md-4" style="padding: 0">
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">	
+			<div class="col-xs-3 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Cliente: </strong></h6>
+			</div>
+			<div class="col-xs-9 col-sm-8 col-md-8 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $idoc->c_firstname }} {{ $idoc->c_lastname }}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-5 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Cédula: </strong></h6>
+			</div>
+			<div class="col-xs-7 col-sm-8 col-md-8 text-center panel panel-default">
+				<h6>{{ number_format($idoc->id_number,0,",",".") }}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-4 col-sm-4 col-md-4 quote-title">	
+				<h6><strong>Teléfono: </strong></h6>
+			</div>
+			<div class="col-xs-8 col-sm-8 col-md-8 text-center panel panel-default">
+				<h6>{{$idoc->phone}}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-2 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Email: </strong></h6>
+			</div>
+			<div class="col-xs-10 col-sm-8 col-md-8 text-center panel panel-default" style="padding: 0; margin: 0">
+				<h6>{{ $idoc->email }}</h6>
+			</div>
 		</div>
 	</div>
 	<div class="col-xs-4 col-sm-4 col-md-4">
-		<div class="col-xs-4 col-sm-4 col-md-4">
-			<p><strong>Cliente: </strong></p>
-			<p><strong>Cédula: </strong></p>
-			<p><strong>Teléfono: </strong></p>
-			<p><strong>Email: </strong></p>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-4 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Vehículo: </strong></h6>
+			</div>
+			<div class="col-xs-8 col-sm-7 col-md-7 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $make->name }} {{ $type->name }}</h6>
+			</div>
 		</div>
-		<div class="col-xs-8 col-sm-8 col-md-8">
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $idoc->c_firstname }} {{ $idoc->c_lastname }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ number_format($idoc->id_number,0,",",".") }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{$idoc->phone}}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $idoc->email }}</p>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-6 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Modelo: </strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-7 col-md-7 text-center panel panel-default">
+				<h6>{{ $idoc->model }}</h6>
+			</div>
 		</div>
-	</div>
-	<div class="col-xs-4 col-sm-4 col-md-4">
-		<div class="col-xs-5 col-sm-5 col-md-5">
-			<p><strong>Vehículo: </strong></p>
-			<p><strong>Modelo: </strong></p>
-			<p><strong>Placa: </strong></p>
-			<p><strong>Kilometraje: </strong></p>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-6 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Placa: </strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-7 col-md-7 text-center panel panel-default">
+				<h6>{{ $idoc->license }}</h6>
+			</div>
 		</div>
-		<div class="col-xs-7 col-sm-7 col-md-7">
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $make->name }} {{ $type->name }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $idoc->model }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $idoc->license }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ number_format($idoc->mileage,0,",",".") }}</p>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-6 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Kilometraje: </strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-7 col-md-7 text-center panel panel-default" style="padding: 0; margin: 0">
+				<h6>{{ number_format($idoc->mileage,0,",",".") }} kms</h6>
+			</div>
 		</div>
 	</div>
 </div>
-<hr>
-<div class="row ">
+<div class="row well quote table-items">
 	<div class="col-xs-6 col-sm-6 col-md-6">
 		@php
 			$k=1;
@@ -246,10 +294,10 @@
 	    @endforeach
 	</div>
 </div>
-<div class="row ">
-	<h5 class="text-center"><strong>Llamamos la atención sobre los siguientes aspectos a realizar al vehículo:</strong></h5>
+<div class="row well quote">
+	<h5 class="text-center" style="margin: 0"><strong>Llamamos la atención sobre los siguientes aspectos a realizar al vehículo:</strong></h5>
 </div>
-<div class="row ">
+<div class="row well quote">
 	<div class="col-xs-12 col-sm-12 col-md-12">
 		<div class="col-xs-4 col-sm-4 col-md-4">
 			<h5 class="text-center"><strong>Semáforo</strong></h5>
@@ -308,7 +356,7 @@
 		</div>
 	</div>
 </div>
-<div class="row" style="margin: 8px 0;">
+<div class="row well quote">
 	<div class="col-xs-8 col-xs-offset-2">
 		<div class="showSig" id="sig-employee">
 			<div id="exp-sig" class="sig sigWrapper">
@@ -321,11 +369,10 @@
 		</div>
 	</div>
 </div>
-<hr style="margin: 3px 0;">
 <div class="row footer">
-	<h6 class="text-center"><strong>NOTA: La inspección visual no implica desarme ni uso de equipos de diagnóstico. Al ser un servicio gratuito el taller no se hace responsable por daños no aparentes ni fallas posteriores a dicha revisión.</strong></h6>
+	<h6 class="text-center"><strong>NOTA: La inspección visual no implica desarme ni uso de equipos de diagnóstico. No se responde por daños no aparentes.</strong></h6>
 </div>
-<hr style="margin: 3px 0;">
+<hr style="margin: 5px 0">
 <div class="row footer">
 	<h6 class="text-center">Carrera 22 N. 76-57 | Bogotá - Colombia | 2117943 - 2119290<br>www.servitalleres.com | contacto@servitalleres.com</h6>
 </div>
--- resources/views/qdocs/pdf.blade.php
+++ resources/views/qdocs/pdf.blade.php
@@ -6,59 +6,106 @@
 		<img src="{{asset('img/logo.png')}}" class="img-responsive" alt="logo servitalleres">
 	</div>
 	<div class="col-xs-6 col-sm-6 col-md-6 text-right text-danger doc-number">
-		<p><strong>No. {{ $doc }}</strong></p>
+		<div class="col-xs-5 col-sm-5 col-md-5 col-xs-offset-7 text-center panel panel-default">
+			<p><strong>No. {{ $doc }}</strong></p>
+		</div>
 	</div>
 </div>
 <div class="row">
 	<h3 class="text-center text-uppercase"><strong>Certificado de Control Calidad @if($qdoc->status == 'cancelled')<span class="text-danger">- ANULADO -</span>@endif </strong></h3>
-	<hr>
 </div>
-<div class="row ">
-	<div class="col-xs-4 col-sm-4 col-md-4">
-		<div class="col-xs-4 col-sm-4 col-md-4">
-			<p><strong>Fecha:</strong></p>
-			<p><strong>Orden:</strong></p>
-			<p><strong>Asesor:</strong></p>
+<div class="row well quote">
+	<div class="col-xs-4 col-sm-4 col-md-4" style="padding: 0">
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="col-xs-6 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Fecha:</strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-8 col-md-8 text-center panel panel-default">
+				<h6>{{ date('d/m/Y', strtotime($qdoc->created_at)) }}</h6>
+			</div>
 		</div>
-		<div class="col-xs-8 col-sm-8 col-md-8">
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ date('d/m/Y', strtotime($qdoc->created_at)) }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $qdoc->ordernumber }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $qdoc->e_firstname }} {{ $qdoc->e_lastname }}</p>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="col-xs-6 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Orden:</strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-8 col-md-8 text-center panel panel-default">
+				<h6>{{ $qdoc->ordernumber }}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<div class="col-xs-3 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Asesor:</strong></h6>
+			</div>
+			<div class="col-xs-9 col-sm-8 col-md-8 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $qdoc->e_firstname }} {{ $qdoc->e_lastname }}</h6>
+			</div>
 		</div>
 	</div>
-	<div class="col-xs-4 col-sm-4 col-md-4">
-		<div class="col-xs-4 col-sm-4 col-md-4">
-			<p><strong>Cliente: </strong></p>
-			<p><strong>Teléfono: </strong></p>
-			<p><strong>Email: </strong></p>
+	<div class="col-xs-4 col-sm-4 col-md-4" style="padding: 0">
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-3 col-sm-4 col-md-4 quote-title">	
+				<h6><strong>Cliente: </strong></h6>
+			</div>
+			<div class="col-xs-9 col-sm-8 col-md-8 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $qdoc->c_firstname }} {{ $qdoc->c_lastname }}</h6>
+			</div>
 		</div>
-		<div class="col-xs-8 col-sm-8 col-md-8">
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $qdoc->c_firstname }} {{ $qdoc->c_lastname }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{$qdoc->phone}}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $qdoc->email }}</p>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-4 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Teléfono: </strong></h6>
+			</div>
+			<div class="col-xs-8 col-sm-8 col-md-8 text-center panel panel-default">
+				<h6>{{$qdoc->phone}}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-2 col-sm-4 col-md-4 quote-title">
+				<h6><strong>Email: </strong></h6>
+			</div>
+			<div class="col-xs-10 col-sm-8 col-md-8 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $qdoc->email }}</h6>
+			</div>
 		</div>
 	</div>
-	<div class="col-xs-4 col-sm-4 col-md-4">
-		<div class="col-xs-5 col-sm-5 col-md-5">
-			<p><strong>Vehículo: </strong></p>
-			<p><strong>Modelo: </strong></p>
-			<p><strong>Placa: </strong></p>
-			<p><strong>Kilometraje: </strong></p>
+	<div class="col-xs-4 col-sm-4 col-md-4" style="padding: 0">
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-4 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Vehículo: </strong></h6>
+			</div>
+			<div class="col-xs-8 col-sm-7 col-md-7 text-center panel panel-default" style="padding: 0">
+				<h6>{{ $make->name }} {{ $type->name }}</h6>
+			</div>
 		</div>
-		<div class="col-xs-7 col-sm-7 col-md-7">
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $make->name }} {{ $type->name }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $qdoc->model }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ $qdoc->license }}</p>
-			<p class="text-center" style="border-bottom: 1px solid #eee;">{{ number_format($qdoc->mileage,0,",",".") }}</p>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-6 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Modelo: </strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-7 col-md-7 text-center panel panel-default">
+				<h6>{{ $qdoc->model }}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-6 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Placa: </strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-7 col-md-7 text-center panel panel-default">
+				<h6>{{ $qdoc->license }}</h6>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12" style="padding: 0 10px">
+			<div class="col-xs-6 col-sm-5 col-md-5 quote-title">
+				<h6><strong>Kilometraje: </strong></h6>
+			</div>
+			<div class="col-xs-6 col-sm-7 col-md-7 text-center panel panel-default" style="padding: 0">
+				<h6>{{ number_format($qdoc->mileage,0,",",".") }} kms</h6>
+			</div>
 		</div>
 	</div>
 </div>
-<div class="row ">
-	<hr>
-	<h4 class="text-center text-uppercase"><strong>Puntos de control</strong></h4>
-	<hr>
+<div class="row well quote">
+	<h4 class="text-center text-uppercase" style="margin: 0"><strong>Puntos de control</strong></h4>
 </div>
-<div class="row ">
+<div class="row well quote table-items">
 	<div class="col-xs-6 col-sm-6 col-md-6">
 		@foreach($names as $mat=>$name)
     		@if($mat < 4)
@@ -182,16 +229,14 @@
 	    @endforeach
 	</div>
 </div>
-<div class="row">
+<div class="row well quote">
 	<h4 class="text-center text-uppercase"><strong>Importante:</strong></h4>
 	<h5 class="text-center"><strong>Los controles realizados son únicamente sobre los elementos visibles del vehículo y no implican desmontaje alguno, por lo tanto el taller no asume responsabilidad en caso de la no detección de una falla no aparente.</strong></h5>
-	<hr>
 </div>
-<div class="row">
+<div class="row well quote">
 	<h5 class="text-center"><strong>Llamamos la atención sobre los siguientes trabajos pendientes de realizar:</strong></h5>
-	<hr>
 </div>
-<div class="row">
+<div class="row well quote">
 	<div class="col-xs-12 col-sm-12 col-md-12">
 		<div class="col-xs-4 col-sm-4 col-md-4">
 			<h5 class="text-center"><strong>Semáforo</strong></h5>
@@ -242,7 +287,7 @@
 		</div>
 	</div>
 </div>
-<div class="row">
+<div class="row well quote">
 	<div class="col-xs-6 col-sm-6 col-md-6 ">
 		<h5 class="text-center"><strong>Conformidad factura/trabajos:</strong></h5>
 		<div class="showSig sig-pdf" id="sig-employee">
@@ -256,11 +301,13 @@
 		</div>
 	</div>
 	<div class="col-xs-6 col-sm-6 col-md-6 ">
-		<div class="col-xs-8 col-sm-8 col-md-8">
-			<h5 class="text-center"><strong>Próximo mantenimiento a los:</strong></h5>
-		</div>
-		<div class="col-xs-4 col-sm-4 col-md-4">
-			<h5 class="text-center" style="border-bottom: 1px solid #eee;"><strong>{{number_format($qdoc->n_mileage,0,",",".")}} kms</strong></h5>
+		<div class="col-xs-12 col-sm-12 col-md-12">	
+			<div class="col-xs-8 col-sm-8 col-md-8 quote-title">
+				<h5><strong>Próximo mantenimiento a los:</strong></h5>
+			</div>
+			<div class="col-xs-4 col-sm-4 col-md-4 text-center panel panel-default" style="padding: 0">
+				<h5>{{number_format($qdoc->n_mileage,0,",",".")}} kms</h5>
+			</div>
 		</div>
 		<div class="showSig sig-pdf" id="sig-client">
 			<div class="sig sigWrapper">
