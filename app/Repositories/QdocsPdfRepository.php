65a98b0e591074ff01914fd63230af63f1ad03a2
Daniel <oscar.d.gonzalez@gmail.com>
2021-04-15 17:10:48 -0500

    deleted zoom property in qdocs pdf to fix display in heroku

--- app/Repositories/QdocsPdfRepository.php
+++ app/Repositories/QdocsPdfRepository.php
@@ -21,7 +21,7 @@ trait QdocsPdfRepository {
                 $elements = Config::get('constants.qdoc_elements');
                 $comments = Config::get('constants.qdoc_comments');
                 $doc = $qdoc->doc_number + 1115;
-		$pdf = \PDF::loadView('qdocs.pdf', compact('qdoc', 'make', 'type', 'names', 'items', 'cats', 'elements', 'comments','doc'))->setPaper('Letter')->setOption('zoom', 1.3)->setOption('margin-left', 6)->setOption('margin-right', 6)->setOption('footer-spacing', 2)->setOption('footer-left', "[doctitle] | Impreso el: [date]")->setOption('footer-right', "Pagina [page] de [toPage]")->setOption('footer-font-size', 7);
+		$pdf = \PDF::loadView('qdocs.pdf', compact('qdoc', 'make', 'type', 'names', 'items', 'cats', 'elements', 'comments','doc'))->setPaper('Letter')->setOption('margin-left', 6)->setOption('margin-right', 6)->setOption('footer-spacing', 2)->setOption('footer-left', "[doctitle] | Impreso el: [date]")->setOption('footer-right', "Pagina [page] de [toPage]")->setOption('footer-font-size', 7);
 
 		return $pdf;
 	}
